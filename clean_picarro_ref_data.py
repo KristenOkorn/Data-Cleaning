2c304046975695dd503b75e5be269336c1a0e293
Kristen Okorn, PhD <47339677+KristenOkorn@users.noreply.github.com>
2024-04-26 19:16:21 +0000

    Update clean_picarro_ref_data.py
    fixes to formatting issues

--- clean_picarro_ref_data.py
+++ clean_picarro_ref_data.py
@@ -1,44 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Sep 13 22:01:15 2022
-
-@author: okorn
-"""
-
-#Import in necessary packages
-from tkinter.filedialog import askdirectory
-import os
-import pandas as pd
-
-#Prompt user to select folder for analysis
-path = askdirectory(title='Select Folder for analysis').replace("/","\\")
-
-#Get the list of files from this directory
-from os import listdir
-from os.path import isfile, join
-fileList = [f for f in listdir(path) if isfile(join(path, f))]
-
-#create a dictionary to hold our data from each file
-data_dict = {}
-
-#loop through each of the files & extract only the columns we need:
-  #datetime, CO2 (wet), CH4 (wet), H2O
-  
-#iterate over each file in the main folder
-for i in range(len(fileList)-1):
-    
-    #Create full file path for reading file
-    filePath = os.path.join(path, fileList[i])
-    
-    #load in the file
-    temp = pd.read_csv(filePath,delim_whitespace=True,usecols=['DATE','TIME','CO2','CH4','H2O'],parse_dates=[['DATE', 'TIME']])
-    
-    #Save this into our data dictionary
-    data_dict['{}'.format(fileList[i])] = temp
-    
 #concatenate all of our data into 1 array
 full_data = pd.concat(data_dict.values())
+
+#Convert 'H2O' column to numeric
+full_data['H2O'] = pd.to_numeric(full_data['H2O'], errors='coerce')
     
+#remove any nans
+full_data = full_data.dropna()
+
 #apply the water vapor correction for co2
 full_data['CO2'] = full_data['CO2'] / (1+(-0.012*full_data['H2O'])+(-0.000267*full_data['H2O']*full_data['H2O']))
 #apply the water vapor correction for ch4
@@ -46,20 +14,33 @@ full_data['CH4'] = full_data['CH4'] / (1+(-0.009823*full_data['H2O'])+(-0.000239
 #delete the water column
 full_data = full_data.drop(columns=['H2O'])
 #apply the calibration correction for co2
-full_data['CO2'] = full_data['CO2'] / 0.9975
+full_data['CO2'] = (full_data['CO2']*0.9955) + 2.5842
 #apply the calibration correction for ch4
-full_data['CH4'] = full_data['CH4'] / 1.0078
+full_data['CH4'] = (full_data['CH4']*-0.0808) + 1.0307
 
 #resample (retime) the data to minutely
 full_data['DATE_TIME'] = pd.to_datetime(full_data['DATE_TIME'])
-full_data = full_data.set_index('DATE_TIME').resample('T').mean()
+#rename the datetime column
+full_data.rename(columns={'DATE_TIME': 'datetime'}, inplace=True)
+#make the datetime the index
+full_data.index = full_data['datetime']
+#drop the original datetime column
+full_data.drop(['datetime'],axis=1,inplace=True)
+#retime
+full_data = full_data.resample("T").mean()
+
+#remove empty rows from retime
+full_data = full_data.dropna()
+
+#Picarro runs 1 min and 2-3 sec slower - account for this
+full_data.index = full_data.index + pd.Timedelta(minutes=1, seconds=2)
 
 #get the final data in a convenient format
-CO2 = pd.DataFrame(full_data.index,full_data['CO2'])
-CH4 = pd.DataFrame(full_data.index,full_data['CH4'])
+CO2 = pd.DataFrame(data={'CO2': full_data['CO2'].values}, index=full_data.index)
+CH4 = pd.DataFrame(data={'CH4': full_data['CH4'].values}, index=full_data.index)
 
 #save out the final data
-savePath = os.path.join(path,'CO2.csv')
+savePath = os.path.join(path,'CO2_2024.csv')
 CO2.to_csv(savePath)
-savePath = os.path.join(path,'CH4.csv')
+savePath = os.path.join(path,'CH4_2024.csv')
 CH4.to_csv(savePath)
